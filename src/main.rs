794fc336fa97410f6a57269a301bee86b7085513
selvaraj <selvaraj.shanmugam@swireshipping.com>
2025-11-07 15:36:18 +0530

    15.1 instruction execution machine

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod vm;
+use vm::*;
 mod chunks;
 use chunks::*;
 mod value;
 
 fn main() {
+    let mut vm = VM::new();
+
     let mut chunk = Chunk::new();
 
     let constant = chunk.add_constant(1.2);
@@ -11,8 +15,9 @@ fn main() {
 
     chunk.write_opcode(OpCode::OpReturn.into(), 123);
     chunk.disassemble("test chunk");
+
+    vm.interpret(&chunk);
+
     chunk.free();
-
-
-
+    vm.free();
 }
